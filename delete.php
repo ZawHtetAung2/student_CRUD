fa1903ff81ad385feffa6006690e6b162222f38b
Zaw Htet Aung <106802492+z3usZ3ro@users.noreply.github.com>
2022-06-06 16:42:45 +0630

    Update delete.php

--- delete.php
+++ delete.php
@@ -18,7 +18,14 @@ if(!empty($values)){
     $userID = $_GET['userid'];
 
     $sqli = "DELETE FROM $myTable
-             WHERE $columns[0] = $userID";  
+             WHERE $columns[0] = $userID"; 
     $mysqli->query($sqli);
+
+    $sqli = "SET @num := 0"; 
+    $mysqli->query($sqli);
+
+    $sqli = "UPDATE $myTable SET $columns[0] = @num := (@num+1);"; 
+    $mysqli->query($sqli);
+
     header("Location:StudentList.php");
 ?>
